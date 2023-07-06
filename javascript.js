4ca85606ec75086743a9be5a7c2c65863a617e42
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2023-07-06 19:24:02 +0100

    new repositor

---
+++ javascript.js
@@ -0,0 +1,7 @@
+
+document.getElementById("meuBotao").addEventListener("click", function() {
+    document.getElementById("container").style.background = URL("assets/4.svg");
+});
+
+alert("eu terminei o basico mas nao coloquei nenhum efeito ainda, falta terminar");
+
